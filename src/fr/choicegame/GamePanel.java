e5e90d1c23af34ebd0036a79039b89757a059ae7
Kleme <klemek@outlook.fr>
2017-02-11 17:37:54 +0100

    Display base code

--- src/fr/choicegame/GamePanel.java
+++ src/fr/choicegame/GamePanel.java
@@ -1,9 +1,45 @@
 package fr.choicegame;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class GamePanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 
+	private Game game;
+	
+	public GamePanel(Game game){
+		
+		this.game = game;
+		
+		Timer refresh = new Timer(20, new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				GamePanel.this.repaint();
+			}
+		});
+		refresh.start();
+
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		if(this.game != null){
+			g2.setColor(Color.BLACK);
+		}else{
+			g2.setColor(Color.DARK_GRAY);
+		}
+		g2.fillRect(0, 0, this.getWidth(), this.getHeight());
+	}
+	
+	
+	
 }
--- src/fr/choicegame/Window.java
+++ src/fr/choicegame/Window.java
@@ -6,8 +6,24 @@ public class Window extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
+	private Game game;
+	private GamePanel gpanel;
+	
+	
 	public Window(Loader loader){
+		this.setTitle("Choice Game");
+		this.setSize(600, 400); //TODO choose window sizes
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+		//this.game = new Game();
+		
+		this.gpanel = new GamePanel(null);
+		
+		this.setContentPane(this.gpanel);
+		
+		this.setVisible(true);
 	}
 	
 	
