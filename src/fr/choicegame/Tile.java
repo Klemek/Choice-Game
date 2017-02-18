e3b2dd903e983afc667f5fe20e2b91e1ee7aa248
minipierre <minipierre@minipierre-N550JK>
2017-02-18 18:42:51 +0100

    Tile base

--- src/fr/choicegame/Tile.java
+++ src/fr/choicegame/Tile.java
@@ -2,8 +2,42 @@ package fr.choicegame;
 
 public class Tile {
 
-	int id;
-	String tileset;
-	Script script;
+	private int id;
+	private String tileset;
+	private Script script;
+	
+	public Tile() {
+		
+	}
+	
+	public Tile(int id, String tileset, Script script) {
+		this.id = id;
+		this.tileset = tileset;
+		this.script = script;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getTileset() {
+		return this.tileset;
+	}
+	
+	public void setTileset(String tileset) {
+		this.tileset = tileset;
+	}
+	
+	public Script getScript() {
+		return this.script;
+	}
+	
+	public void setScript(Script script) {
+		this.script = script;
+	}
 	
 }
