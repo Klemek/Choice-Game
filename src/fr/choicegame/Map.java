8129c4b1922db71d1d10dd374a24704e52e5626b
minipierre <minipierre@minipierre-N550JK>
2017-02-18 19:12:14 +0100

    J'ai merdé loul

--- src/fr/choicegame/Map.java
+++ src/fr/choicegame/Map.java
@@ -3,6 +3,22 @@ package fr.choicegame;
 public class Map {
 	
 	Tile[][] map;
-	String name;	
+	String name;
+	
+	public Tile[][] getMap() {
+		return this.map;
+	}
+	
+	public void setMap(Tile[][] map) {
+		this.map = map;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}	
 	
 }
