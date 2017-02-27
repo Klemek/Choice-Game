8c85ab84b2c811fc24b11343f04d2462f6612bfd
Kalioz <Kalioz@192.168.1.55>
2017-02-27 22:14:43 +0100

    ajout méthodes statiques loadImageFile and loadTextFile

--- src/fr/choicegame/Loader.java
+++ src/fr/choicegame/Loader.java
@@ -1,6 +1,9 @@
 package fr.choicegame;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 
@@ -9,8 +12,9 @@ import javax.imageio.ImageIO;
 
 public class Loader {
 	
-	HashMap<String,BufferedImage> resources = new HashMap<String,BufferedImage>();
-		
+	HashMap<String,BufferedImage> imagesResources = new HashMap<String,BufferedImage>();
+	HashMap<String,String> textResources = new HashMap<String,String>();
+	
 	/**
 	 * load all pictures resources contained in the res folder
 	 * files contained in another folder under /res/ WON'T be loaded
@@ -27,7 +31,7 @@ public class Loader {
 			  //ajouter vérification type image
 			  
 			  try{
-				  resources.put(listOfFiles[i].getName(), ImageIO.read(listOfFiles[i]));
+				  imagesResources.put(listOfFiles[i].getName(), ImageIO.read(listOfFiles[i]));
 				  System.out.println("File " + listOfFiles[i].getName() + " loaded");
 				  
 			  }catch(IOException e){
@@ -39,19 +43,64 @@ public class Loader {
 		return true;
 	}
 	
+	public static BufferedImage loadImageAsset(String path){
+		try{
+			return ImageIO.read(new File(path));
+		}catch(IOException e){return null;}
+	}
 	
+	public static String loadTextAsset(String pathToSource){
+		try
+		  {
+		      File f = new File (pathToSource);
+		      FileReader fr = new FileReader (f);
+		      BufferedReader br = new BufferedReader (fr);
+		      String content = "";
+		   
+		      try
+		      {
+		          String line = br.readLine();  
+		          while (line != null)
+		          {
+		        	  content = content + line + "\n";
+		              line = br.readLine();
+		          }
+		          br.close();
+		          fr.close();
+		          return content;
+		      }
+		      catch (IOException exception)
+		      {
+		          System.out.println ("Erreur lors de la lecture : " + exception.getMessage());
+		      }
+		  }
+		  catch (FileNotFoundException exception)
+		  {
+		      System.out.println ("Le fichier n'a pas été trouvé");
+		  }
+		return null;
+	}
+	
+	public HashMap<String, String> getGameFiles(){
+		return this.textResources;
+	}
+	
+	public String getGameFile(String name){
+		return this.textResources.get(name);
+	}
 	
 	public HashMap<String,BufferedImage> getGameAssets(){
-		return this.resources;
+		return this.imagesResources;
 	}
 	
 	/**
 	 * get the asset corresponding to name
 	 * @param name the name of the resource
-	 * @return the image if existing, null if not.
+	 * @return the image 
+	 * if existing, null if not.
 	 */
 	public BufferedImage getGameAsset(String name){		
-		return this.resources.get(name);
+		return this.imagesResources.get(name);
 	}
 	
 }
