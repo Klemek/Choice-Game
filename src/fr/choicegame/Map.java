d6daac4719a270226913ca95c4fdede6ee808a4c
minipierre <minipierre@minipierre-N550JK>
2017-03-05 17:02:03 +0100

    COrrection

--- src/fr/choicegame/Map.java
+++ src/fr/choicegame/Map.java
@@ -17,9 +17,7 @@ public class Map {
 			}
 		}
 	}
-	
-	// Getters & Setters
-	
+		
 	public void setGameEventListener(GameEventListener listener) {
 		for(int x = 0; x < getWidth(); x++) {
 			for(int y = 0; y < getHeight(); x++) {
