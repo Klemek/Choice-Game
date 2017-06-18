fbfd04ca210eab33b9b8dc55622fb01d18b5286a
Kalioz <Kalioz@pc-kalioz>
2017-06-18 12:13:13 +0200

    ajout cartes maison

---
+++ src/main/resources/maps/type.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="type" tilewidth="32" tileheight="32" tilecount="2" columns="2">
+ <image source="typetilseset.png" width="64" height="32"/>
+ <tile id="0">
+  <properties>
+   <property name="type" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="type" type="int" value="2"/>
+  </properties>
+ </tile>
+</tileset>
